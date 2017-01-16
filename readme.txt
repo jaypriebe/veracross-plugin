053e0f6d36ea396ee61a96127feca19adba8fd0f
Joe Guilmette <joe@mbp.local>
2017-01-16 17:38:18 +0700

    hello

---
+++ readme.txt
@@ -0,0 +1,4 @@
+=== Veracross Staff Members ===
+Stable tag: .1
+
+Create Staff Members as a post type for import from Veracross
---
+++ veracross.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Veracross Staff Members
+Description: Create Staff Members as a post type for import from Veracross
+Author: NIST
+Version: .1
+License: GPLv2+
+ */
